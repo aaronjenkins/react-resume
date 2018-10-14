8d5059792d2bde413ea50e7974198a409e70bd4b
Aaron Jenkins <aaronjenkins@outlook.com>
2018-10-14 17:36:34 -0400

    init

--- src/App.js
+++ src/App.js
@@ -9,15 +9,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Aaron Jenkins
           </p>
           <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+            className = 'App-link'
+            href="mailto:aaron@aaronjenkins.me">
+            Contact
           </a>
         </header>
       </div>
