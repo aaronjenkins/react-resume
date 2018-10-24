8e11e38f2bec318649e7962819935bc792e2ca17
Aaron Jenkins <aaronjenkins@outlook.com>
2018-10-23 21:48:27 -0400

    add card components

--- src/components/work.js
+++ src/components/work.js
@@ -1,6 +1,7 @@
 import { FaBriefcase } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import { FaDotCircle } from "react-icons/fa";
+import { CardStack, Card } from "react-cardstack";
 
 const React = require("react");
 
@@ -11,37 +12,39 @@ export class Work extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.props.work.map(function(listValue) {
-          return (
-            <React.Fragment>
-              <ul>
-                <FaBriefcase className="App-icons" />
-                <a href={listValue.website}>{listValue.company}</a>
-                <ul>
-                  <li>{listValue.position}</li>
-                  <li>
-                    {listValue.startDate}
-                    <FaArrowRight className="App-icons" />
-                    {listValue.endDate}
-                  </li>
-                  <li>Highlights: </li>
-                  {listValue.highlights.map(function(highlight) {
-                    return (
-                      <React.Fragment>
-                        <ul>
-                          <li>
-                            <FaDotCircle className="App-icons" />
-                            {highlight}
-                          </li>
-                        </ul>
-                      </React.Fragment>
-                    );
-                  })}
-                </ul>
-              </ul>
-            </React.Fragment>
-          );
-        })}
+        <CardStack>
+          {this.props.work.map(function(listValue) {
+            return (
+                <Card background={listValue.color}>
+                  <ul>
+                    <FaBriefcase className="App-icons" />
+                    <a href={listValue.website}>{listValue.company}</a>
+                    <ul>
+                      <li>{listValue.position}</li>
+                      <li>
+                        {listValue.startDate}
+                        <FaArrowRight className="App-icons" />
+                        {listValue.endDate}
+                      </li>
+                      <li>Highlights: </li>
+                      {listValue.highlights.map(function(highlight) {
+                        return (
+                          <React.Fragment>
+                            <ul>
+                              <li>
+                                <FaDotCircle className="App-icons" />
+                                {highlight}
+                              </li>
+                            </ul>
+                          </React.Fragment>
+                        );
+                      })}
+                    </ul>
+                  </ul>
+                </Card>
+            );
+          })}
+        </CardStack>
       </React.Fragment>
     );
   }
