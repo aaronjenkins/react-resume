dc361c9392bfb72b9a7bc1238b4ddfda662e0f80
Aaron Jenkins <aaron@aaronjenkins.me>
2020-01-10 13:03:28 -0500

    1/8 column size

--- src/App.js
+++ src/App.js
@@ -1,68 +1,102 @@
-import React, { Component } from "react"
-import logo from "./me.jpg"
-import "./App.scss"
-import data from "./resume.json";
-import { Grid, Row, Column } from "react-cellblock"
-import { FaDraftingCompass } from "react-icons/fa"
-import { FaCode } from "react-icons/fa"
-import { FaToolbox } from "react-icons/fa"
-import { Email } from "./components/email"
-import { LinkedIn } from "./components/linkedin"
-import { GitHub } from "./components/github"
-import { WorkAccordion } from "./components/workaccordion.js"
-import { FaBriefcase } from "react-icons/fa"
-import {FaEdit} from "react-icons/fa"
+import React, { Component } from 'react'
+import logo from './me.jpg'
+import './App.scss'
+import data from './resume.json'
+import { Grid, Row, Column } from 'react-cellblock'
+import { FaDraftingCompass } from 'react-icons/fa'
+import { FaCode } from 'react-icons/fa'
+import { FaToolbox } from 'react-icons/fa'
+import { Email } from './components/email'
+import { LinkedIn } from './components/linkedin'
+import { GitHub } from './components/github'
+import { WorkAccordion } from './components/workaccordion.js'
+import { FaBriefcase } from 'react-icons/fa'
+import { FaEdit } from 'react-icons/fa'
 
-const Name = require("./components/name");
-const List = require("./components/list");
+const Name = require('./components/name')
+const List = require('./components/list')
 
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Grid className="App-header">
-          <Row>
-            <Column width="3/3">
-        <div className="App">
-          <header className="App-header">
-            <Name name={data.basics.name} />
-            <img src={logo} className="App-logo" alt="logo" />
-            <Email email={data.basics.email} />
-            <div>
-              <GitHub url={data.basics.github} />
-              <LinkedIn url={data.basics.linkedIn} />
-            </div>
-          </header>
-        </div>
-        </Column>
-        </Row>
-        </Grid>
-        <Grid>
-          <Row>
-            <Column width="1/3" className="App-column">
-              <FaCode className="App-icons" />
-              <List name="Languages" list={data.languages} />
-            </Column>
-            <Column width="1/3" className="App-column">
-              <FaDraftingCompass className="App-icons" />
-              <List name="Technologies" list={data.technologies} />
-            </Column>
-            <Column width="1/3" className="App-column">
-              <FaToolbox className="App-icons" />
-              <List name="Tools" list={data.tools} />
-            </Column>
-          </Row>
-          <Row>
-            <span class='App-title-with-icon'><FaEdit className="App-icons-large" />Projects</span>
-          </Row>
-          <Row>
-            <span class='App-title-with-icon'> <FaBriefcase className="App-icons-large" />Work History</span>
-            <WorkAccordion work={data.work}  />
-          </Row>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Grid className="App-header">
+                    <Row>
+                        <Column width="3/3">
+                            <div className="App">
+                                <header className="App-header">
+                                    <Name name={data.basics.name} />
+                                    <img
+                                        src={logo}
+                                        className="App-logo"
+                                        alt="logo"
+                                    />
+                                    <Email email={data.basics.email} />
+                                    <div>
+                                        <GitHub url={data.basics.github} />
+                                        <LinkedIn url={data.basics.linkedIn} />
+                                    </div>
+                                </header>
+                            </div>
+                        </Column>
+                    </Row>
+                </Grid>
+                <Grid>
+                    <Row>
+                        <Column width="1/8" className="App-column" />
+                        <Column width="2/8" className="App-column">
+                            <ul className="App-ul">
+                                <li>
+                                    <FaCode className="App-icons" />
+                                    {'Languages'}
+                                </li>
+                                <List name="Languages" list={data.languages} />
+                            </ul>
+                        </Column>
+                        <Column width="2/8" className="App-column">
+                            <ul className="App-ul">
+                                <li>
+                                    <FaDraftingCompass className="App-icons" />
+                                    {'Technologies'}
+                                </li>
+                                <List
+                                    name="Technologies"
+                                    list={data.technologies}
+                                />
+                            </ul>
+                        </Column>
+                        <Column width="2/8" className="App-column">
+                        <ul className="App-ul">
+                                <li>
+                                    <FaToolbox className="App-icons" />
+                                    {'Tools'}
+                                </li>
+                                <List
+                                    name="Tools"
+                                    list={data.tools}
+                                />
+                            </ul>
+                        </Column>
+                        <Column width="1/8" className="App-column" />
+                    </Row>
+                    <Row>
+                        <span class="App-title-with-icon">
+                            <FaEdit className="App-icons-large" />
+                            Projects
+                        </span>
+                    </Row>
+                    <Row>
+                        <span class="App-title-with-icon">
+                            {' '}
+                            <FaBriefcase className="App-icons-large" />
+                            Work History
+                        </span>
+                        <WorkAccordion work={data.work} />
+                    </Row>
+                </Grid>
+            </React.Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/components/list.js
+++ src/components/list.js
@@ -3,7 +3,6 @@ const React = require('react')
 const List = ({ name, list}) => {
     return (
         <React.Fragment>
-            <span className='App-list-title'>{name}</span>
             <ul className = "App-ul">
                 {list.map(function(listValue) {
                     return <li>{listValue.name}</li>
